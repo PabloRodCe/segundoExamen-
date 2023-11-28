ac83aea6a51d04e947f9dd8a5a70fc35ebc3b423
pablo rodriguez <jorodriguezce@est.utn.ac.vr>
2023-11-28 10:00:58 -0600

    se crea el metodo
     se añade el metodo atendido

--- src/AreaEmergencias/ListaPacientes.java
+++ src/AreaEmergencias/ListaPacientes.java
@@ -11,14 +11,44 @@ import java.util.ArrayList;
  * @author Student
  */
 public class ListaPacientes {
-    private ArrayList<Pacientes>listaPacientes= new ArrayList<>();
-    
-    public void AgregarPaciente(int id, String nombre, String fecha, String horaLlegada, String horaSalida, String clasificacion){
-        if (listaPacientes.isEmpty()) {
-            listaPacientes.add(Pacientes);
-            
+
+    private ArrayList<Pacientes> listaPacientes = new ArrayList<>();
+
+    public void AgregarPaciente(int id, String nombre, String fecha, String horaLlegada, String horaSalida, String clasificacion) {
+        Pacientes paciente = new Pacientes();
+        if (listaPacientes.isEmpty() && paciente.getClasificacion().equals("rojo")) {
+            listaPacientes.add(paciente);
+
+        } else if (paciente.getClasificacion().equals("amrillo") && listaPacientes.isEmpty()) {
+            listaPacientes.add(paciente);
+
+        } else if (paciente.getClasificacion().equals("verde") && listaPacientes.isEmpty()) {
+            listaPacientes.add(paciente);
         }
-        
+
     }
-public void 
+
+    public void atendido(String horaSalida, int id) {
+        Pacientes paciente = new Pacientes();
+        for (Pacientes listaPaciente : listaPacientes) {
+            
+        
+
+        if (paciente.getId() == id && paciente.getHoraSalida().equals(horaSalida)) {
+            listaPacientes.remove(paciente.getId());
+
+        }
+    }
+    }
+
+    public void buscarPaciente(int id) {
+        Pacientes paciente = new Pacientes();
+        for (Pacientes listaPaciente : listaPacientes) {
+            if (paciente.getId() == id) {
+
+            }
+            return;
+        }
+    }
+
 }
